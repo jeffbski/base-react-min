78ed18d792acf518dce11156015a0c1ad10b2370
Jeff Barczewski <jeff.barczewski@gmail.com>
2015-04-29 12:43:45 -0500

    add iso-http and fake data api

--- src/browser.jsx
+++ src/browser.jsx
@@ -1,19 +1,42 @@
 'use strict';
 
 import React, { Component } from 'react';
+import http from 'iso-http';
 
 const mainContentDiv = document.querySelector('#appContainer');
 
 class App extends Component {
   render() {
+    const items = this.props.items;
     return (
-      <div>App content</div>
+      <div>
+        <ul>
+          { items.map(item =>
+            <li key={item.id}>{item.name}</li>) }
+        </ul>
+        <div className="devHelp">(from src/browser.jsx)</div>
+      </div>
     );
   }
 }
 
-function render() {
-  React.render(<Handler/>, mainContentDiv);
+function render(data) {
+  React.render(<App items={data.items} />, mainContentDiv);
 }
 
-render();
+function fetchData(cb) {
+  http.request({ url: '/fake-api.json' }, resp => {
+    if (status > 399) { return cb(new Error(resp.text)); }
+    try {
+      const data = JSON.parse(resp.text);
+      return cb(null, data);
+    } catch (err) {
+      return cb(new Error(err));
+    }
+  });
+}
+
+fetchData(function (err, data) {
+  if (err) { return console.error(err); }
+  render(data);
+});
