d9a1c9f94116df52e4248668445323068a78d691
Jeff Barczewski <jeff.barczewski@gmail.com>
2015-09-14 11:27:15 -0500

    use postcss, karma

--- bs-config.js
+++ bs-config.js
@@ -1,6 +1,10 @@
+
+// http://www.browsersync.io/docs/options/
+
 module.exports = {
   browser: ['google chrome'],
   // browser: ['google chrome canary'],
+  ghostMode: false,
   port: 3005,
   // proxy: 'localhost:8005',
   reloadDelay: 500,
